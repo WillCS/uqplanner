a10893692953878cef76bd5696c62f957e3bfb4d
WillCS <wcstibbards@gmail.com>
2019-09-24 21:49:04 +1000

    Implemented some cql setup.

--- api/src/calendar/calendar.py
+++ api/src/calendar/calendar.py
@@ -5,113 +5,19 @@ from cassandra.cqlengine.usertype import UserType
 class ClassSession(UserType):
     start_time  = columns.Time()
     end_time    = columns.Time()
-    location    = columns.Ascii()
-
-class ClassSessionRecurring(ClassSession):
+    location    = columns.Text()
     day         = columns.Integer()
 
-class ClassSessionOneOff(ClassSession):
-    day         = columns.Date()
-
 class ClassStream(UserType):
-    classes     = columns.List(columns.UserDefinedType(ClassSession))
+    weeks       = columns.Set(columns.Integer())
+    classes     = columns.Set(columns.UserDefinedType(ClassSession))
 
 class Class(UserType):
-    name        = columns.Ascii()
-    streams     = columns.Map(columns.Integer(), columns.UserDefinedType(ClassStream))
+    name        = columns.Text()
+    streams     = columns.List(columns.UserDefinedType(ClassStream))
 
 class SubjectOffering(Model):
-    name        = columns.Ascii(primary_key = True)
+    name        = columns.Text(primary_key = True)
     year        = columns.Integer(primary_key = True)
     semester    = columns.Integer(primary_key = True)
-    classes     = columns.List(columns.UserDefinedType(Class))
-
-def get_infs():
-    return {
-        'name': 'INFS3208',
-        'classes': [
-            {
-                'name': 'L',
-                'streams': [
-                    {
-                        'classes': [
-                            {
-                                'day': 4,
-                                'startTime': { 'hours': 12, 'minutes': 0 },
-                                'endTime': { 'hours': 14, 'minutes': 0 },
-                                'location': 'Forgan Smith (01) E215'
-                            }
-                        ]
-                    }
-                ]
-            }, {
-                'name': 'P',
-                'streams': [
-                    {
-                        'classes': [
-                            {
-                                'day': 0,
-                                'startTime': { 'hours': 12, 'minutes': 0 },
-                                'endTime': { 'hours': 14, 'minutes': 0 },
-                                'location': 'General Purpose South(78)208'
-                            }
-                        ]
-                    }, {
-                        'classes': [
-                            {
-                                'day': 0,
-                                'startTime': { 'hours': 14, 'minutes': 0 },
-                                'endTime': { 'hours': 16, 'minutes': 0 },
-                                'location': 'General Purpose South(78)208'
-                            }
-                        ]
-                    }, {
-                        'classes': [
-                            {
-                                'day': 2,
-                                'startTime': { 'hours': 12, 'minutes': 0 },
-                                'endTime': { 'hours': 14, 'minutes': 0 },
-                                'location': 'General Purpose South(78)208'
-                            }
-                        ]
-                    }, {
-                        'classes': [
-                            {
-                                'day': 3,
-                                'startTime': { 'hours': 8, 'minutes': 0 },
-                                'endTime': { 'hours': 10, 'minutes': 0 },
-                                'location': 'General Purpose South(78)208'
-                            }
-                        ]
-                    }, {
-                        'classes': [
-                            {
-                                'day': 3,
-                                'startTime': { 'hours': 10, 'minutes': 0 },
-                                'endTime': { 'hours': 12, 'minutes': 0 },
-                                'location': 'General Purpose South(78)208'
-                            }
-                        ]
-                    }, {
-                        'classes': [
-                            {
-                                'day': 3,
-                                'startTime': { 'hours': 14, 'minutes': 0 },
-                                'endTime': { 'hours': 16, 'minutes': 0 },
-                                'location': 'General Purpose South(78)208'
-                            }
-                        ]
-                    }, {
-                        'classes': [
-                            {
-                                'day': 4,
-                                'startTime': { 'hours': 8, 'minutes': 0 },
-                                'endTime': { 'hours': 10, 'minutes': 0 },
-                                'location': 'General Purpose South(78)208'
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
-    }
+    classes     = columns.Set(columns.UserDefinedType(Class))
--- api/src/database/orm.py
+++ api/src/database/orm.py
@@ -1,5 +1,27 @@
 from cassandra.cluster import Cluster
 from cassandra.cqlengine import columns
 
-def get_cluster(ips, port) -> Cluster:
-    cluster = Cluster(ips, port = port)
+from util import get_config, cache
+from calendar import Class, ClassSession, ClassStream
+
+@cache
+def get_cluster() -> Cluster:
+    """
+    Get the cluster to connect to. Because we only ever want to connect to a
+    single cassandra cluster, the output of this function is cached.
+
+    Effectively, the cluster is a singleton.
+    """
+    config = get_config()
+
+    ips = config['db_addresses']
+    port = config['db_port']
+    keyspace = config['db_keyspace']
+
+    cluster: Cluster = Cluster(ips, port = port)
+
+    cluster.register_user_type(keyspace, 'class_session', ClassSession)
+    cluster.register_user_type(keyspace, 'class_stream', ClassStream)
+    cluster.register_user_type(keyspace, 'class', Class)
+
+    return cluster
