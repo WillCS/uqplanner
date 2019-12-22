b42d55ee544373aaa6a84e30affa23f7c99014bf
Darren Fu <darren@Darrens-MacBook-Pro.local>
2019-12-23 01:41:08 +1000

    added a cors proxy, hooked up app to uq public api'

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ClassListing, Semester } from './calendar/calendar';
+import { ClassListing, ClassType, ClassStream, ClassSession, Semester, WEEKDAYS } from './calendar/calendar';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -8,16 +8,28 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class ApiService {
-  private api = 'http://localhost:2727';
+  private api = 'http://localhost:8010/proxy/even/rest/timetable';
 
   constructor(private http: HttpClient) {
 
   }
 
   public getClass(courseCode: string, year: number, semester: number): Observable<ClassListing> {
-    const endpoint: string = this.endpoint('test');
-    return this.http.get<string>(endpoint).pipe(map(
-        (value: string) => JSON.parse(value)));
+    const endpoint: string = this.endpoint('subjects');
+
+    return this.http.post<JSON>(
+      endpoint,
+      `search-term=${courseCode}&semester=ALL&campus=ALL&faculty=ALL&type=ALL&days=1&days=2&days=3&days=4&days=5&days=6&days=0&start-time=00%3A00&end-time=23%3A00`,
+      {
+        headers: {
+          "accept": "application/json, text/javascript, */*; q=0.01",
+          "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+          "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+          "x-requested-with": "XMLHttpRequest"
+        }
+      }).pipe(
+        map((classObj: JSON) => this.reformatClass(courseCode, classObj))
+      );
   }
 
   public getActiveSemesters(): Observable<Semester[]> {
@@ -29,4 +41,48 @@ export class ApiService {
   private endpoint(name: string): string {
     return `${this.api}/${name}`;
   }
+
+  private reformatClass(courseCode: string, obj: JSON): ClassListing {
+    let name = Object.keys(obj)
+      .filter(key => !key.includes("_EX"))
+      .find(key => key.split('_')[0] == courseCode);
+    
+    let activities = Object.values(obj[name].activities).reduce((acc: Object, val: Object) => {
+      acc.hasOwnProperty(val['activity_group_code']) 
+        ? acc[val['activity_group_code']].push(val) 
+        : acc[val['activity_group_code']] = [val];
+      return acc;
+    }, {});
+
+    let classes = Object.values(activities).map((act: any): ClassType => {
+      return {
+        name: act[0]['activity_group_code'].split("_")[0],
+        streams: [{
+          classes: act.map((s: Object): ClassSession => ({
+            day: WEEKDAYS.findIndex(d => d.startsWith(s['day_of_week'])),
+            startTime: {
+              hours: parseInt(s['start_time'].split(":")[0]), 
+              minutes: parseInt(s['start_time'].split(":")[1])
+            },
+            endTime: {
+              hours: parseInt(s['start_time'].split(":")[0]) + parseInt(s['duration']) / 60, 
+              minutes: parseInt(s['start_time'].split(":")[1]) + parseInt(s['duration']) % 60
+            },
+            location: s['location']
+          }))
+        }]
+      }
+    });
+
+    let classList = {
+      name: name.split("_")[0],
+      classes: classes
+    };
+
+    console.log(classList);
+
+    return classList;
+  }
 }
+
+
