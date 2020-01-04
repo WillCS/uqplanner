3b25f148406391013dbe3967e6b45e15f5965b5e
Darren Fu <darrenfu@outlook.com>
2020-01-04 22:37:49 +1000

    fix word breaks within blocks, fix donate link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   proxyAddress: 'https://lingering-bush-c27d.late-night.workers.dev',
   apiAddress: 'https://timetable.my.uq.edu.au/even/rest/timetable',
-  donateLink: 'https://www.paypal.com/donate/?token=DKEAWv4RheWbcVXP5EPMca9yuaH4PXyqeOw6eCYrm-cizAPjRSx6uQAsc51y7E_Q1m5UzW&country.x=US&locale.x='
+  donateLink: 'https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=devs%40uqplanner.app&currency_code=AUD&source=url'
 };
 
 /*
