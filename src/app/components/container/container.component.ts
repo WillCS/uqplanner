8527db5f18d8d8488dbd7541f735bb11a0d5f043
Darren Fu <darrenfu@outlook.com>
2020-01-04 18:23:53 +1000

    moved donate link to env

--- src/app/components/container/container.component.ts
+++ src/app/components/container/container.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef, TemplateRef } from '@angular/core';
 import { ModalService } from '../modal/modal.service';
 import { ModalSettings, ModalButton } from '../modal/modal';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-container',
@@ -10,6 +11,8 @@ import { ModalSettings, ModalButton } from '../modal/modal';
 export class ContainerComponent implements OnInit {
   @ViewChild('feedback', { read: TemplateRef, static: false })
   private feedbackTemplate: TemplateRef<any>;
+
+  public donateLink = environment.donateLink;
 
   constructor(private modalService: ModalService) {
 
